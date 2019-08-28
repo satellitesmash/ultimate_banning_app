2e4248a6e362fd385f15ef1bdc7904275cb2ce6c
Jonathan Cady <joncady@uw.edu>
2019-08-27 19:18:53 -0700

    Finished first pass

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Stage from './Stage';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import { states, resetObj } from './states';
 
 export default class App extends Component {
 
 	constructor(props) {
 		super(props);
+		let url = new URL(window.location);
+		let blankBackground = url.searchParams.get("hide");
+		let hide = false;
+		if (blankBackground) {
+			document.querySelector("body").classList.add("transparent");
+			hide = true;
+		}
 		this.state = {
 			stageEls: null,
-			counterpick: false,
+			counterpick: true,
 			stageStates: null,
-			color: "blue"
-		}
+			loading: true,
+			hide: hide
+		};
 	}
 
 	componentDidMount() {
@@ -35,13 +44,15 @@ export default class App extends Component {
 			this.setState({
 				counterpicks: counterpicks,
 				starters: starters,
-				stageStates: values
+				stageStates: values,
 			});
 		});
 		this.counterpick.on("value", (snapshot) => {
-			let value = snapshot.val();
+			let data = snapshot.val();
 			this.setState({
-				counterpick: value === "starter" ? false : true
+				counterpick: data.value === "starter" ? false : true,
+				stateCount: data.count,
+				loading: false
 			});
 		});
 	}
@@ -52,23 +63,63 @@ export default class App extends Component {
 	}
 
 	alterState = (id, lastValue) => {
-		let value = lastValue === "blue" ? "none" : "blue";
-		let dataObject = {
-			value: value
-		};
-		this.stageRef.child(id).update(dataObject);
+		const { stateCount, counterpick } = this.state;
+		if (lastValue === "none" && ((counterpick && stateCount <= 3) || (!counterpick && stateCount <= 5))) {
+			let stateObj = counterpick ? states.counterpick : states.starter;
+			let dataObject = {
+				value: stateObj["state" + stateCount]
+			};
+			let dataObject2 = {
+				count: stateCount + 1
+			};
+			this.stageRef.child(id).update(dataObject);
+			this.counterpick.update(dataObject2);
+		}
+	}
+
+	switchMode = () => {
+		let dataObj = {
+			value: !this.state.counterpick ? "counterpick" : "starter"
+		}
+		this.counterpick.update(dataObj);
+	}
+
+	reset = () => {
+		this.counterpick.update({
+			count: 1,
+			value: "starter"
+		});
+		this.stageRef.set(resetObj);
 	}
 
 	render() {
-		const { starters, counterpick, counterpicks } = this.state;
+		const { starters, counterpick, counterpicks, loading, hide } = this.state;
 		return (
 			<div className="App">
-				<div id="stage-container">
-					{starters}
-					{counterpick &&
-						counterpicks
-					}
-				</div>
+				{loading ?
+					<div></div>
+					:
+					<div>
+						<div id="stage-container">
+							{starters}
+							{counterpick &&
+								counterpicks
+							}
+						</div>
+						<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+							<div className={hide ? "hide" : ""} style={{ width: '30%'}}>
+								<button className="button" style={{ margin: '1rem '}} onClick={this.switchMode}>Swap</button>
+								<button className="button" style={{ margin: '1rem '}} onClick={this.reset}>Reset</button>
+							</div>
+							<div>
+								<img height={100} alt="pp5-logo" src={require('./pp5.png')}></img>
+							</div>
+							<div style={{ width: '30%', textAlign: 'right' }}>
+								<h2 style={{ color: 'white', margin: '1rem' }}>{counterpick ? "counterpick".toUpperCase() : "starter".toUpperCase()}</h2>
+							</div>
+						</div>
+					</div>
+				}
 			</div>
 		);
 	}
---
+++ src/states.js
@@ -0,0 +1,60 @@
+exports.states = {
+
+    starter: {
+        state1: "blue",
+        state2: "red",
+        state3: "red",
+        state4: "blue",
+        state5: "green"
+    },
+
+    counterpick: {
+        state1: "blue",
+        state2: "blue",
+        state3: "green"
+    }
+
+};
+
+exports.resetObj = {
+    '-L_RU2xGK4KiUOgFkool': {
+        name: 'battlefield.jpg',
+        value: 'none'
+    },
+    '-L_RU2xLA-bFgTH7-N50': {
+        name: 'final_destination.jpg',
+        value: 'none'
+    },
+    '-L_RU2xMnWVajCL8zLtK': {
+        counterpick: true,
+        name: 'kalos.png',
+        value: 'none'
+    },
+    '-L_RU2xMnWVajCL8zLtL': {
+        counterpick: true,
+        name: 'lylat.jpg',
+        value: 'none'
+    },
+    '-L_RU2xNGG37M1hMOpvQ': {
+        name: 'ps2.png',
+        value: 'none'
+    },
+    '-L_RU2xNGG37M1hMOpvR': {
+        name: 'smashville.png',
+        value: 'none'
+    },
+    '-L_RU2xNGG37M1hMOpvS': {
+        name: 'town.png',
+        value: 'none'
+    },
+    '-L_RU2xO1Wj5IZQ9iNW1': {
+        counterpick: true,
+        name: 'yoshi_story.png',
+        value: 'none'
+    }
+};
+
+exports.resetCounterpick = {
+    count: 1,
+    value: "starter"
+}
