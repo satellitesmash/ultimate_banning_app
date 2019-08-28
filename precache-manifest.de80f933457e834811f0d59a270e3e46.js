0388f2f9e179408cb62870938785a8a536820c70
Jonathan Cady <joncady@uw.edu>
2019-08-27 19:20:34 -0700

    Updates

---
+++ precache-manifest.de80f933457e834811f0d59a270e3e46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "64987f6ca44ae911e379ea232fbb40c1",
+    "url": "/ultimate_banning_app/static/media/pp5.64987f6c.png"
+  },
+  {
+    "revision": "ddc2f1f9db3f0c1e30525a28e7a6043f",
+    "url": "/ultimate_banning_app/static/media/bannedRed.ddc2f1f9.png"
+  },
+  {
+    "revision": "254370afdfd4b4a1c376844a1feb4b61",
+    "url": "/ultimate_banning_app/static/media/Nexa-Bold.254370af.otf"
+  },
+  {
+    "revision": "014de568f144b8c77c58",
+    "url": "/ultimate_banning_app/static/js/runtime~main.3998073a.js"
+  },
+  {
+    "revision": "540fd01314f96d1d78a5",
+    "url": "/ultimate_banning_app/static/js/main.ce79a42d.chunk.js"
+  },
+  {
+    "revision": "2a025d0bca44f1214ec8",
+    "url": "/ultimate_banning_app/static/js/2.bbc9120a.chunk.js"
+  },
+  {
+    "revision": "540fd01314f96d1d78a5",
+    "url": "/ultimate_banning_app/static/css/main.829c08d2.chunk.css"
+  },
+  {
+    "revision": "84d29d3d68c005c1f1fa7379ff1116bf",
+    "url": "/ultimate_banning_app/index.html"
+  }
+];
